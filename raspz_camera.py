3ea891881112df7442a19edd4230cadf3df7877e
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-21 00:54:20 +0200

    Adjusted the code for the camera for practical use. This includes refactoring the code in a function with flexible parameters and adjusting settings of camera preview. The optimal camera settings (like guo did) still have to be implemented.

---
+++ raspz_camera.py
@@ -0,0 +1,23 @@
+from picamera import PiCamera
+import time
+
+
+def take_picture(picture_name: str, flip_camera: bool, resolution: tuple, sleep_time: float,
+                 folder_name="trash_pictures", window_pos_size=(1400, 0, 500, 500), conf_msg=True):
+    """Take a picture with the camera and save it in the "trash_pictures" folder."""
+    camera = PiCamera()
+    camera.resolution = resolution
+    camera.vflip = flip_camera
+
+    camera.start_preview(fullscreen=False, window=window_pos_size)
+
+    time.sleep(sleep_time)
+    camera.capture(f"{folder_name}/{picture_name}.jpeg")
+
+    camera.stop_preview()
+
+    if conf_msg:
+        print("picture taken")
+
+
+take_picture(picture_name="picture1", flip_camera=True, resolution=(640, 480), sleep_time=2)
