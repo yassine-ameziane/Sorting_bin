3a9157e1160c51f215f5ca8af400105d08d5dcab
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-26 16:09:10 +0200

    change ultrasonic code to raspberry pi

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from raspz_camera import take_picture
 
 # use function to see whether there is an object:
-from machine import Pin
+import RPi.GPIO as GPIO
+import time
 from ultrasonic import set_pins, get_distance
 
 # make a prediction on the category using the neural network:
@@ -10,24 +11,28 @@ from neural_network import get_image, load_model, predict_category
 # move the waste to the correct bin:
 from motor_sorting import sort_waste
 
-trigger, echo = set_pins(pin_out=3, pin_in=2)
+# initiate variables and set pin settings
+set_pins(trigger_pin=16, echo_pin=18)
 categories_dict = {0: "other", 1: "plastic"}
 model = False
 
-while True:
-    distance_object = get_distance(trigger_out=trigger, echo_in=echo)
-    trash_is_present = distance_object < 5  # choose threshold distance
+try:
+    while True:
+        distance_object = get_distance(trigger_out=trigger, echo_in=echo)
+        trash_is_present = distance_object < 5  # choose threshold distance
 
-    if trash_is_present:
-        take_picture(picture_name="picture1", flip_camera=True, resolution=(640, 480), sleep_time=2)
-        image = get_image(picture_name="picture1", Target_size=(224, 224))
+        if trash_is_present:
+            take_picture(picture_name="picture1", flip_camera=True, resolution=(640, 480), sleep_time=2)
+            image = get_image(picture_name="picture1", Target_size=(224, 224))
 
-        if not model:  # load the model if it hasn't be loaded yet
-            model = load_model()
+            if not model:  # load the model if it hasn't be loaded yet
+                model = load_model()
 
-        trash_category = predict_category(model1=model, img_array=image)  # 0: other trash, 1: plastic
-        sort_waste(pin_nr=, category=categories_dict[trash_category], error_bool=True)
+            trash_category = predict_category(model1=model, img_array=image)  # 0: other trash, 1: plastic
+            sort_waste(pin_nr=, category=categories_dict[trash_category], error_bool=True)
 
-        time.sleep(10)  # wait for a few seconds
-    else:
-        time.sleep(1)
+            time.sleep(10)  # wait for a few seconds
+        else:
+            time.sleep(1)
+finally:
+    GPIO.cleanup()
--- ultrasonic.py
+++ ultrasonic.py
@@ -1,28 +1,44 @@
-from machine import Pin
-import utime
+import RPi.GPIO as GPIO
+import time
 
 
-def set_pins(pin_out: int, pin_in):
-    trigger_out = Pin(pin_out, Pin.OUT)
-    echo_in = Pin(pin_in, Pin.IN)
-    return trigger_out, echo_in
+def set_pins(trigger_pin: int, echo_pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(echo_pin, GPIO.IN)
 
 
 def get_distance(trigger_out, echo_in):
     """Returns distance of object from ultrasonic sensor in cm."""
-    trigger_out.low()
-    utime.sleep_us(2)
-    trigger_out.high()
-    utime.sleep_us(5)
-    trigger_out.low()
-    while echo_in.value() == 0:
-        signaloff = utime.ticks_us()
-    while echo_in.value() == 1:
-        signalon = utime.ticks_us()
-    timepassed = signalon - signaloff
-    distance = (timepassed * 0.0343) / 2
-    print("The distance from object is ", distance, "cm")  # this is not needed for our product
-    return distance
+    GPIO.output(trigger_out, GPIO.LOW)
+    time.sleep(0.01)
+    GPIO.output(trigger_out, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(trigger_out, GPIO.LOW)
 
-trigger_out, echo_in = set_pins(pin_out = 16, pin_in=18)
-distance = get_distance(trigger_out, echo_in)
+    while GPIO.input(echo_in) == 0:
+        time1 = time.time()
+    while GPIO.input(echo_in) == 1:
+        time2 = time.time()
+
+    time_difference = time2 - time1
+    distance_cm = (time_difference * 34300) / 2  # in cm
+
+    return distance_cm
+
+
+def check_distance_loop(trigger, echo, period: int, pause=1):
+    """Check the distance for a certain amount of time and print it."""
+    set_pins(trigger_pin=trigger, echo_pin=echo)
+
+    try:
+        for i in range(period):
+            distance = get_distance(trigger_out=trigger, echo_in=echo)
+            print("The distance from object is ", distance, "cm")
+            time.sleep(pause)
+    finally:
+        GPIO.cleanup()
+        print("cleaned GPIO-setup")
+
+
+check_distance_loop(trigger=16, echo=18, period=30, pause=1)
