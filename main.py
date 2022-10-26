ebb5974a217aa680a3482d09f104e8d83d04cf79
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-26 15:51:24 +0200

    expanded the main.py file

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from raspz_camera import take_picture
 
 # use function to see whether there is an object:
-# from ultrasonic import object_close
+from machine import Pin
+from ultrasonic import set_pins, get_distance
 
 # make a prediction on the category using the neural network:
-# from neural_network import predict_category
+from neural_network import get_image, load_model, predict_category
 
 # move the waste to the correct bin:
-# from motor_sorting import sort_waste
+from motor_sorting import sort_waste
 
+trigger, echo = set_pins(pin_out=3, pin_in=2)
+categories_dict = {0: "other", 1: "plastic"}
+model = False
 
 while True:
-    trash_is_present = object_close()
+    distance_object = get_distance(trigger_out=trigger, echo_in=echo)
+    trash_is_present = distance_object < 5  # choose threshold distance
 
     if trash_is_present:
         take_picture(picture_name="picture1", flip_camera=True, resolution=(640, 480), sleep_time=2)
-        trash_category = predict_category()  # 0: other trash, 1: plastic
-        sort_waste(trash_category)
-        time.sleep(10) # wait for a few seconds
+        image = get_image(picture_name="picture1", Target_size=(224, 224))
+
+        if not model:  # load the model if it hasn't be loaded yet
+            model = load_model()
+
+        trash_category = predict_category(model1=model, img_array=image)  # 0: other trash, 1: plastic
+        sort_waste(pin_nr=, category=categories_dict[trash_category], error_bool=True)
+
+        time.sleep(10)  # wait for a few seconds
+    else:
+        time.sleep(1)
