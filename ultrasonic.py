bb491d9cb4e0fa0307477fddf6e6d4580c9b6b11
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-26 15:49:28 +0200

    reformat ultrasonic code

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,23 +1,28 @@
 from machine import Pin
 import utime
 
-trigger = Pin(3, Pin.OUT)
-echo = Pin(2, Pin.IN)
 
-def ultra():
-    trigger.low()
+def set_pins(pin_out: int, pin_in):
+    trigger_out = Pin(pin_out, Pin.OUT)
+    echo_in = Pin(pin_in, Pin.IN)
+    return trigger_out, echo_in
+
+
+def get_distance(trigger_out, echo_in):
+    """Returns distance of object from ultrasonic sensor in cm."""
+    trigger_out.low()
     utime.sleep_us(2)
-    trigger.high()
+    trigger_out.high()
     utime.sleep_us(5)
-    trigger.low()
-    while echo.value() == 0:
+    trigger_out.low()
+    while echo_in.value() == 0:
         signaloff = utime.ticks_us()
-    while echo.value() == 1:
+    while echo_in.value() == 1:
         signalon = utime.ticks_us()
     timepassed = signalon - signaloff
     distance = (timepassed * 0.0343) / 2
-    print("The distance from object is ",distance,"cm") # this is not needed for our product
+    print("The distance from object is ", distance, "cm")  # this is not needed for our product
+    return distance
 
-while True:
-    ultra()
-    utime.sleep(1)
+trigger_out, echo_in = set_pins(pin_out = 16, pin_in=18)
+distance = get_distance(trigger_out, echo_in)
